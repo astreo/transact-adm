099a167c65fb03224eafc8cac42d63041effdb8d
Rodrigo Astrada <rodrigo.astrada@gmail.com>
2020-11-09 18:56:14 -0300

    index.html actualizado14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', component: AppComponent },
+  { path: '1', component: TransactionsComponent },
   { path: '**', redirectTo: '' }
 ];
 
